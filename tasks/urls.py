5c7b4c5c37486f9568efac5949fe2403d1c4406e
rozit019 <rozitaryal70@gmail.com>
2025-07-24 07:48:04 +0545

    3rd commit

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('tasks/',views.task_list, name='task_list'),
     path('tasks/create/', views.task_create, name='task_create'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='tasks/logout.html'), name='logout'),
+
 ]
